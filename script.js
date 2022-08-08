99e503fff735c7f0193d5520cdf2bf3ad062d456
José Orlando de Carvalho Júnior <juniororlandocv@gmail.com>
2022-08-08 16:34:17 -0300

    m1-s3-entrega-praticando-com-variaveis-JUorlando

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = 'Jhon'
+let sobrenome = 'Doyle'
+let anoNascimento = 1991
+let idade = 2022 - anoNascimento
+let cidade = 'Curitiba'
+let mensagemDoJhon = `Eu sou ${nome} ${sobrenome} tenho ${idade} anos de idade e moro na cidade de ${cidade}.`
+console.log(mensagemDoJhon)
